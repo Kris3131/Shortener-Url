1738a9bd669874e599bb8a244a6e3b48a6368d8b
Kris <krischiang0301@gmail.com>
2022-08-08 21:04:30 +0800

    chore: set up views using express-handlebars

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
 app.listen(PORT, () => {
 	console.log(`localhost -> http://localhost:${PORT}`)
