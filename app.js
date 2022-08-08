0167f174634d6a174c1d9b3be1d7f52c52fc3173
Kris <krischiang0301@gmail.com>
2022-08-08 21:29:16 +0800

    chore: set up mongoose connected to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('err', () => console.log(`db connected err`))
+db.once('open', () => console.log(`db connected success`))
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
