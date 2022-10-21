64a4829df1a056235aa1bbcf46ecb9407e70dcc4
Kris <krischiang0301@gmail.com>
2022-10-21 18:45:50 +0800

    feat: add register page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const users = require('./modules/users')
 const urlShortener = require('./modules/urlShortener')
 
 const router = express.Router()
 
+router.use('/users', users)
 router.use('/', urlShortener)
-
 module.exports = router
