df9f843573bf0c1a11f711224956a77b7f8ee223
Kris <krischiang0301@gmail.com>
2022-10-23 21:29:07 +0800

    feat: Add session option

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const methodOverride = require('method-override')
 const session = require('express-session')
-require('dotenv').config
-require('./config/mongoose')
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
 
+}
+require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(express.static('publics'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(session({
+const sessionOption ={
 	secret: process.env.SESSION_SECRET,
 	resave: false,
-	saveUninitialized: true
-}))
+	saveUninitialized: true,
+}
+if(app.get('env') === 'production') {
+	app.set('trust proxy',1)
+	sessionOption.cookie.secure = true
+}
+app.use(session(sessionOption))
 app.use(routes)
 
 app.listen(PORT, () => {
