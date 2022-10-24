dcf07795d2d167003b09d0dced3e0a32285eb335
Kris <krischiang0301@gmail.com>
2022-10-24 15:34:56 +0800

    feat: install passport

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')
 
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
+  require('dotenv').config()
 }
 require('./config/mongoose')
 const app = express()
@@ -23,15 +23,15 @@ app.set('view engine', 'hbs')
 app.use(express.static('publics'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-const sessionOption ={
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: true,
-	store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI})
+const sessionOption = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
 }
-if(app.get('env') === 'production') {
-	app.set('trust proxy',1)
-	sessionOption.cookie.secure = true
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1)
+  sessionOption.cookie.secure = true
 }
 app.use(session(sessionOption))
 app.use(routes)
