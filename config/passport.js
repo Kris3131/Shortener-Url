b7d880300f68fb0b0e9b0999cabb95e93f6ecb72
Kris <krischiang0301@gmail.com>
2022-10-27 11:37:54 +0800

    refactor: move google callback url to env

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 async function (accessToken, refreshToken, profile, done) {
   const { name, email } = profile._json
